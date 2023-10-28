09f613f84382b0c9434d01b07cd45648e0fdd8b7
Tony Duong <tony.duong.102@gmail.com>
2023-10-28 23:23:43 +0900

    :sparkles: update credentials

--- config/initializers/google_cloud_vision.rb
+++ config/initializers/google_cloud_vision.rb
@@ -1,5 +1,5 @@
 require "google/cloud/vision"
 
 Google::Cloud::Vision.configure do |config|
-  config.credentials = JSON.parse(File.read(Rails.root.join("config", "vision_credentials.json")))
+  config.credentials = JSON.parse(File.read(Rails.root.join("config", "google-cloud-vision-credentials.json")))
 end
