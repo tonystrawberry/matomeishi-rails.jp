987c05f27cd9660b6e788f3975b997b569aac55c
Tony Duong <tony.duong.102@gmail.com>
2023-10-31 21:08:06 +0900

    :sparkles: add localhost:3002 to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -23,4 +23,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
+
+  allow do
+    origins 'http://localhost:3002'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
 end
